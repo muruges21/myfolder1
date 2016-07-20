7ffd72ed07446fadff1de8c43b1b9e771623a07c
muruges21 <muruges2017@gmail.com>
2016-07-20 15:17:19 +0530

    Add files via upload

---
+++ NumberOfDigitOfInt.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class NumberOfDigitOfInt {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Integer  value : ");
+		String convertIntoString=sc.nextLine();
+		System.out.println("Number of Digit of given number is.: "+convertIntoString.length());
+
+	}
+
+}

739bb029b2f8e3321eae5b94f7c42472a4745d94
muruges21 <muruges2017@gmail.com>
2016-07-24 10:03:52 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,13 @@
+package weee;
+public class str {
+public static void main(String[] args) {
+
+		String s1 = "w1e2l3c4o5m6e7";
+		String h[]=s1.split("[a-z]");
+		for(String s:h)
+		System.out.println(s);
+		    }
+
+	
+
+}
